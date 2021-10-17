c417d954337d5bbdcdb8d3655fa5a9b2d870a4ff
Erdélyi Kristóf <erdelyi.kristof@gmail.com>
2021-10-17 13:50:54 +0200

    Grid layout with floats

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php get_header(); ?>
+<div class="row">
 
 <?php while (have_posts(  )) {
     the_post(  ); ?>
-    <h1><a href="<?php the_permalink(  ); ?>"><?php the_title( ); ?></a></h1>
-    <article><?php the_excerpt(  ) ?></article>
-    <?php
+        <div class="col-1-of-2">
+            <h1><a href="<?php the_permalink(  ); ?>"><?php the_title( ); ?></a></h1>
+            <article><?php the_excerpt(  ) ?></article>
+        </div>
+        <?php
 }; ?>
 
+</div>
 <?php get_footer(); ?>
